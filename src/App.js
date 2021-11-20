809e90ab1aff6af36ab116a5954035c0a27b8ec3
Nikhil Sai Allam <nikhil.logicwind@gmail.com>
2021-11-20 18:56:51 +0530

    :rocket: getting started with react kendo UI

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+// import 'antd/dist/antd.less';
+import '@progress/kendo-react-material/dist/all.css';
+import Kendo from './kendo react/Kendo';
+
+function App() {
+  return (
+    <div className="App">
+      <Kendo />
+    </div>
+  );
+}
+
+export default App;
